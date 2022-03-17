657190ede0ce2d04e91c58d9edeec2e32f9a87d7
Nick McMillen <nick.mcmillen@student.awesomeinc.org>
2022-03-17 18:50:41 +0000

    food api pulling and displaying data in Appetizers

--- src/routes/Appetizers.js
+++ src/routes/Appetizers.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function Appetizers () {
-    return (
-        <div>
-            <h1>Welcome to the Appetizers page</h1>
-        </div>
-    )
-}
+
+const ShowAppetizer = () => {
+  const [appetizers, setAppetizers] = useState([]);
+        useEffect(() => {
+        fetchAppetizers();
+        }, []);
+    const fetchAppetizers = () => {
+    axios
+    .get('https://sheltered-refuge-85246.herokuapp.com/api/json')
+    .then((res) => {
+      console.log("apps", res);
+      setAppetizers(res.data)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+return (
+    <div>
+      <h1>Appetizers List</h1>
+      <div className='item-container'>
+        {appetizers.map((displayApps) => (
+            <div className='card'>
+            <h2>{displayApps.title}</h2>
+            <h4>{displayApps.price}</h4>
+            <p>{displayApps.description}</p>
+            <p>{displayApps.cuisine.label}</p>
+            </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ShowAppetizer;
